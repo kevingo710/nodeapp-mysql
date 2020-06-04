a9b319dd768444c9d7cf96fac56ee647182cca58
kevingo710 <kevin.gracia.tic@gmail.com>
2020-06-04 16:28:26 -0500

    fronted changes

--- src/app/routes/news.js
+++ src/app/routes/news.js
@@ -35,6 +35,35 @@ module.exports = app => {
   });
 
 
+  app.get('/vista1', (req, res) => {
+    connection.query('SELECT * FROM VISTA_SESIONES', (err, rows, fields) => {
+      if(!err) {
+        res.json(rows);
+      } else {
+        console.log(err);
+      }
+    });  
+  });
+
+  app.get('/vista2', (req, res) => {
+    connection.query('SELECT * FROM VISTA_MOSTPLAYED', (err, rows, fields) => {
+      if(!err) {
+        res.json(rows);
+      } else {
+        console.log(err);
+      }
+    });  
+  });
+
+  app.get('/vista3', (req, res) => {
+    connection.query('SELECT * FROM VISTA_SAME_GENRE', (err, rows, fields) => {
+      if(!err) {
+        res.json(rows);
+      } else {
+        console.log(err);
+      }
+    });  
+  });
 
   // app.post('/news', (req, res) => {
   //   const {title, news} = req.body;
